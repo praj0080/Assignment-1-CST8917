f1aa85e3e1d48f213b6773ddb717b7c1bdac796a
degu0055 <degu0055@algonquinlive.com>
2025-07-10 05:14:14 -0400

    working local

--- README.md
+++ README.md
@@ -3,4 +3,12 @@
 Repo:
 https://github.com/degu0055/25S_CST8917_Assignment_1
 
+Query: 
+https://portal.azure.com/#@AlgonquinLivecom.onmicrosoft.com/resource/subscriptions/cdb9bdf3-e7ee-43e9-8f6c-ba7327868df1/resourceGroups/CST8917-rg/providers/Microsoft.Sql/servers/cst8917sql134/databases/cst8917db/connectionStrings
+SELECT * FROM ImageMetadata;
+
+
+Container:
+https://portal.azure.com/#view/Microsoft_Azure_Storage/ContainerMenuBlade/~/overview/storageAccountId/%2Fsubscriptions%2Fcdb9bdf3-e7ee-43e9-8f6c-ba7327868df1%2FresourceGroups%2FCST8917-rg%2Fproviders%2FMicrosoft.Storage%2FstorageAccounts%2Fcst8917storagexxx/path/images-input/etag/%220x8DDBF8945DA7D4D%22/defaultId//publicAccessVal/None
+
 -->
---
+++ function_app.py
@@ -0,0 +1,83 @@
+import logging
+import os
+from datetime import datetime
+
+import azure.functions as func
+import azure.durable_functions as df
+from azure.storage.blob import BlobServiceClient
+from PIL import Image
+import io
+import pyodbc
+
+my_app = df.DFApp(http_auth_level=func.AuthLevel.ANONYMOUS)
+
+blob_service_client = BlobServiceClient.from_connection_string(os.environ.get("AzureWebJobsStorage"))
+
+@my_app.blob_trigger(arg_name="myblob", path="images-input", connection="AzureWebJobsStorage")
+@my_app.durable_client_input(client_name="client")
+async def blob_trigger(myblob: func.InputStream, client):
+    logging.info(f"Blob trigger processed blob: Name={myblob.name}, Size={myblob.length} bytes")
+    blobName = myblob.name.split("/")[-1]
+    await client.start_new("orchestrator", client_input=blobName)
+
+@my_app.orchestration_trigger(context_name="context")
+def orchestrator(context: df.DurableOrchestrationContext):
+    blobName = context.get_input()
+
+    retry_options = df.RetryOptions(first_retry_interval_in_milliseconds=5000, max_number_of_attempts=3)
+
+    metadata = yield context.call_activity_with_retry("extract_metadata", retry_options, blobName)
+    yield context.call_activity_with_retry("store_metadata", retry_options, metadata)
+
+    return f"Metadata processed and stored for {blobName}"
+
+@my_app.activity_trigger(input_name='blobName')
+def extract_metadata(blobName):
+    logging.info(f"Extracting metadata for {blobName}")
+    container_client = blob_service_client.get_container_client("images-input")
+    blob_client = container_client.get_blob_client(blobName)
+    blob_bytes = blob_client.download_blob().readall()
+
+    with Image.open(io.BytesIO(blob_bytes)) as img:
+        metadata = {
+            "FileName": blobName,
+            "FileSizeKB": round(len(blob_bytes) / 1024, 2),
+            "Width": img.width,
+            "Height": img.height,
+            "Format": img.format
+        }
+    logging.info(f"Extracted metadata: {metadata}")
+    return metadata
+
+@my_app.activity_trigger(input_name='metadata')
+def store_metadata(metadata):
+    logging.info(f"Storing metadata in SQL DB: {metadata}")
+
+    conn_str = (
+        "Driver={ODBC Driver 18 for SQL Server};"
+        "Server=tcp:cst8917sql134.database.windows.net,1433;"
+        "Database=cst8917db;"
+        "Uid=sqladminuser;"
+        "Pwd=StrongP@ssword123!;"
+        "Encrypt=yes;"
+        "TrustServerCertificate=no;"
+        "Connection Timeout=30;"
+    )
+
+    try:
+        conn = pyodbc.connect(conn_str)
+        cursor = conn.cursor()
+        insert_query = """
+            INSERT INTO ImageMetadata (FileName, FileSizeKB, Width, Height, Format)
+            VALUES (?, ?, ?, ?, ?)
+        """
+        cursor.execute(insert_query, metadata["FileName"], metadata["FileSizeKB"], metadata["Width"], metadata["Height"], metadata["Format"])
+        conn.commit()
+        cursor.close()
+        conn.close()
+        logging.info("Metadata stored successfully.")
+    except Exception as e:
+        logging.error(f"Failed to store metadata: {e}")
+        raise
+
+    return "Metadata stored successfully."
